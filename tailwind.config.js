d94fb3268c3dfe86cd5d6c523aeb8fae0f75b120
Harshita <harshita.tailang@sparxitsolutions.com>
2024-04-19 17:15:23 +0530

    portfolio project setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    // responsive breakpoints
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'992px',
+      xl:'1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
